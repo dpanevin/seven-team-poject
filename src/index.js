ed1d1bc111b7d3f96e4c6a0e42d8a7c3b5149215
Denis Panevin <d.panevin@gmail.com>
2021-06-08 18:33:09 +0300

    Исправляет ошибки

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import './sass/main.scss';
-import { addSpinner, stopSpinner } from './spinner';
+import { addSpinner, stopSpinner } from './js/spinner';
 // вызывай функции addSpinner() или stopSpinner(), чтобы запустить или остановить спиннер
