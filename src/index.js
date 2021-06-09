aaff53aeece8e3869697191cd2fccb3209fabee2
Ihor Andrieiev <andr.igorrom@gmail.com>
2021-06-09 03:53:15 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import './sass/main.scss';
 import { addSpinner, stopSpinner } from './js/spinner';
 // вызывай функции addSpinner() или stopSpinner(), чтобы запустить или остановить спиннер
+const l = document.querySelector(".modal");
+import filmcard from './templates/filmcard-modal.hbs';
+import MoviesApi from './js/api-service';
+const movieapi = new MoviesApi();
+
+const r = movieapi.fetchMovieById("34335");
+console.log(r);
