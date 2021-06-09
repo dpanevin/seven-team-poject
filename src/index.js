93aeffd8bf192f369f7082ffaafea76317ed6bc5
TretiakOleksandr <zukaramba@gmail.com>
2021-06-10 00:26:07 +0300

    более цивильно

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import './sass/main.scss';
-// import { addSpinner, stopSpinner } from './js/spinner';
+import { addSpinner, stopSpinner } from './js/spinner';
 // вызывай функции addSpinner() или stopSpinner(), чтобы запустить или остановить спиннер
 
 import markupRender from './js/markupRender';
 // markupRender(filmCards); - здесь filmCards - объект JSON, который прилетает в ответ на успешный запрос
 
-
-//---- заполнил страницу популярными фильмами для вида (можно удалять) ---------------------
+//---- заполнил страницу популярными фильмами ---------------------
 import API from './js/api-service';
-
-const qwe = new API;
-
-qwe.fetchTrendingMovies().then(x => {
-    console.log(x);
-    markupRender(x);
-});
-//---- конец импровизации
+const movie = new API;
+movie.fetchTrendingMovies().then(x => markupRender(x));
+//-----------------------------------------------------------------
