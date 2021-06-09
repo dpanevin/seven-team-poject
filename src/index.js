1379082998e0125c7791d5de81a762839d6ca23d
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-06-09 22:30:27 +0300

    сделала динамику header
    localStorage не додумала как включить

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './sass/main.scss';
 import { addSpinner, stopSpinner } from './js/spinner';
 // вызывай функции addSpinner() или stopSpinner(), чтобы запустить или остановить спиннер
+import './js/header'
