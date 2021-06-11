5430fdc350e607602ed8f5b51aa466bf49b04381
Ihor Andrieiev <andr.igorrom@gmail.com>
2021-06-11 21:40:20 +0300

    svg1

--- src/js/modal.js
+++ src/js/modal.js
@@ -48,9 +48,47 @@ e();
 //========================================================
 //
 //     const refs = {
-//       openModalById: document.querySelector('[data-attribute=id]'),
+//       openModalByIdCard: document.querySelector('[data-attribute=id]'),
 //       closeModalBtn: document.querySelector('[data-modal-close]'),
-//       modal: document.querySelector('[data-modal]'),
+//       modal: document.querySelector('.modal'),
+//       backdrop: document.querySelector('[data-modal]'),
+//       modalCard = document.querySelector( `.filmcard` );
 //     };
 
-//     refs.openModalByIdImg.addEventListener('click', onOpenModalById);
+//     refs.openModalByIdCard.addEventListener('click', onOpenModalByIdCard);
+//
+//     const movieapi = new MoviesApi();
+
+//             async function e() {
+//             const renderId = await movieapi.fetchMovieById("id");
+//             const markup = filmcard(renderId);
+//             modal.insertAdjacentHTML('beforeend', markup);;
+//             return renderId;
+//            }
+//
+//    function onOpenModalByIdCard(e) {
+//           event.preventDefault();
+//        if( event.target.className === `data-attribute=id` ) {
+//         refs.backdrop.classList.remove('is-hidden');
+//         e();
+//         }
+//    }
+//
+//     refs.closeModalBtn.addEventListener('click', closeModal);
+//
+//     document.addEventListener( `keyup`, event => {
+//         if( event.key === `Escape` ) {
+//             closeModal();
+//         }
+//     } );
+
+// modalDiv.addEventListener( `click`, ( event ) => {
+//     if( event.target != modalCard ) {
+//         closeModalWindow();
+//     }
+// } );
+
+// function closeModal () {
+//     refs.backdrop.classList.add('is-hidden');
+//     refs.modal.innerHTML = ''
+// };
