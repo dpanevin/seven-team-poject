3d2dfcbfcca4c98e0b0ab4c568cf0da9aca69aca
TretiakOleksandr <zukaramba@gmail.com>
2021-06-14 17:41:19 +0300

    Жанры отображаются словами
    - добавлена ещё одна функция в api-service;
    -  дописана homePageRender, которая и меняет ID жанров на имена;

--- src/js/home.js
+++ src/js/home.js
@@ -1,9 +1,29 @@
 export default function homePageRender(API, fn) {
     const movie = new API;
-    
+
     try {
-        movie.fetchTrendingMovies().then(fn);
+        movie.fetchGenres().then(gnr => {
+            movie.fetchTrendingMovies().then(mvs => {
+                fn(mvs); // рендер (колбек)
+
+                const genreIdsArray = mvs.results.map(card => card.genre_ids);
+                // массив (карточек) с массивами ID жанров
+
+                const genreNamesArray = genreIdsArray.map(film => film.map(genre => getNameById(gnr.genres, genre).name));
+                // массив (карточек) с массивами имен жанров
+                
+                const genreEl = document.querySelectorAll('.film__genre');
+                // строка жанров
+
+                genreEl.forEach((el, ind) => el.textContent = genreNamesArray[ind].join(', '));
+                // меняем циферки на кошерные имена
+            });
+        });
     } catch (error) {
         console.log('Error ',error);
     }    
 }
+
+function getNameById(arr, id) {
+    return arr.find(x => x.id === id);
+} 
