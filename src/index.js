72f9753eb770a65ed6a8993786017febb8568205
Denis Panevin <d.panevin@gmail.com>
2021-06-15 05:20:35 +0300

    Исправляет index.js

--- src/index.js
+++ src/index.js
@@ -5,21 +5,13 @@ import { addSpinner, stopSpinner } from './js/spinner';
 // вызывай функции addSpinner() или stopSpinner(), чтобы запустить или остановить спиннер
 import './js/header';
 import markupRender from './js/markupRender';
-// markupRender(filmCards); - здесь filmCards - объект JSON, который прилетает в ответ на успешный запрос
-
-//---- заполнил страницу популярными фильмами ---------------------
-
 import homePageRender from './js/home';
 import './js/header'
+import onSearch from './js/search';
+import './js/modal';
+import './js/library';
+import './js/pagination';
 
 
 homePageRender(movieAPI, markupRender);
-//-------------------------------------------------------------
 
-//-------------------------------------------------------------
-import onSearch from './js/search';
-// MODAL ======================================
-import './js/modal'
-const moviesApi = new API();
-import './js/pagination'
-import './js/library';
