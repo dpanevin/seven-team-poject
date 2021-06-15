e427e6f248b36107ddc643122de31d6d0ef7eef6
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-06-15 14:33:04 +0300

    corrects modal.js

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,65 +1,56 @@
-
 import filmcard from './../templates/filmcard-modal.hbs';
 import MoviesApi from './api-service';
-
 
 //===========================================================
 
 const movieapi = new MoviesApi();
 
-
-
 //======================================================
-    const refs1 = {
-      openModalByIdCard: document.querySelector('.card__set'),
-      closeModalBtn: document.querySelector('[data-modal-close]'),
-      modal: document.querySelector('.modal'),
-      backdrop: document.querySelector('.backdrop'),
-        modalCard: document.querySelector('.filmcard'),
-        bodyEl: document.querySelector('body')
-    
+const refs1 = {
+  openModalByIdCard: document.querySelector('.card__set'),
+  closeModalBtn: document.querySelector('[data-modal-close]'),
+  modal: document.querySelector('.modal'),
+  backdrop: document.querySelector('.backdrop'),
+  modalCard: document.querySelector('.filmcard'),
+  bodyEl: document.querySelector('body'),
 };
-   
 
-     refs1.openModalByIdCard.addEventListener('click', onClickCard);
+refs1.openModalByIdCard.addEventListener('click', onClickCard);
 
 function onClickCard(evt) {
+  const idCard = evt.target.dataset.attribute;
 
-    const idCard = evt.target.dataset.attribute;
-  
-    refs1.backdrop.classList.toggle('is-hidden');
-    refs1.bodyEl.classList.add('modal-open');
+  refs1.backdrop.classList.toggle('is-hidden');
+  refs1.bodyEl.classList.add('modal-open');
 
-    async function e() {
-        
-      
+  async function e() {
     const renderId = await movieapi.fetchMovieById(idCard);
-        // const markup = ;
-       
+    // const markup = ;
+
     refs1.modal.insertAdjacentHTML('beforeend', filmcard(renderId));
     return renderId;
+  }
+  e().then(response => {
+    const btnEl = document.querySelector('.btnwatched');
+    console.log(btnEl);
+  });
 }
-    e();
 
-}
-
-document.addEventListener( `keyup`, e => {
-        if( e.key === `Escape` ) {
-            closeModal();
-        }
-    } );
+document.addEventListener(`keyup`, e => {
+  if (e.key === `Escape`) {
+    closeModal();
+  }
+});
 
 function closeModal() {
-    refs1.bodyEl.classList.remove('modal-open');
-    refs1.backdrop.classList.add('is-hidden');
-        const modalCard = document.querySelector('.filmcard')
+  refs1.bodyEl.classList.remove('modal-open');
+  refs1.backdrop.classList.add('is-hidden');
+  const modalCard = document.querySelector('.filmcard');
 
-if (modalCard) {  
-    modalCard.remove()  
+  if (modalCard) {
+    modalCard.remove();
+  }
 }
-    
-     
-};
 
- refs1.closeModalBtn.addEventListener('click', closeModal);
+refs1.closeModalBtn.addEventListener('click', closeModal);
 closeModal();
