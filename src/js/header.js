fc1434903fc25bcd16387953da957d3f593897fd
Ivanka1991 <73844703+Ivanka1991@users.noreply.github.com>
2021-06-17 00:20:38 +0300

    исправила замечания от Димы
    стили на авторизации еще не придумала как поправить
    ссылки перенесла в общий файл

--- src/js/header.js
+++ src/js/header.js
@@ -1,17 +1,18 @@
 import homePageRender from './home';
 import MovieAPI from './api-service';
 import markupRender from './markupRender';
-
+import getRefs from './refs';
 const movieAPI = new MovieAPI();
-
-const refs = {
+const refs = getRefs();
+/* const refs = {
   navHome: document.querySelector('.home'),
   navLibr: document.querySelector('.libr'),
   searchLine: document.querySelector('.search__line'),
   libBtn: document.querySelector('.library__btn'),
   carSetEl: document.querySelector('.card__set'),
-  logoEl: document.querySelector(".logo")
-};
+  logoEl: document.querySelector(".logo"),
+  header: document.querySelector(".header"),
+}; */
 refs.navLibr.addEventListener('click', onLibrClick);
 function onLibrClick(event) {
   event.preventDefault();
@@ -19,16 +20,21 @@ function onLibrClick(event) {
   refs.libBtn.classList.remove('visually-hidden');
   refs.navLibr.classList.add('current');
   refs.navHome.classList.remove('current');
+  refs.header.classList.remove('header');
+  refs.header.classList.add('library');
   refs.carSetEl.innerHTML = '';
 }
 
 refs.logoEl.addEventListener('click', onRenderPage);
+refs.navHome.addEventListener('click',onRenderPage );
 
-function onRenderPage(){
+function onRenderPage(event){
   event.preventDefault();
   refs.navHome.classList.add('current');
   refs.navLibr.classList.remove('current');
   refs.libBtn.classList.add('visually-hidden');
   refs.searchLine.classList.remove('visually-hidden');
+  refs.header.classList.add('header');
+  refs.header.classList.remove('library');
   homePageRender(movieAPI, markupRender);
 }
