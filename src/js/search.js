2ae8c523a73e4ea18a67b3881225d183ee613b84
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-06-18 13:15:06 +0300

    corrects notification on an empty search field input

--- src/js/search.js
+++ src/js/search.js
@@ -1,4 +1,4 @@
-import createMarkup from './markupRender'
+import createMarkup from './markupRender';
 import { alert } from '@pnotify/core';
 import '@pnotify/core/dist/PNotify.css';
 import '@pnotify/core/dist/BrightTheme.css';
@@ -6,67 +6,62 @@ import getRefs from './refs';
 
 const refs = getRefs();
 
-
 export { onSearch, renderSearchPage };
 
 async function onSearch(e, filmsApi) {
+  const form = e.currentTarget;
 
-    const form = e.currentTarget;
-    
-    filmsApi.query = form.elements.query.value;
+  filmsApi.query = form.elements.query.value;
 
-    // form.reset();
-    filmsApi.resetPage();
+  // form.reset();
+  filmsApi.resetPage();
 
-    await renderSearchPage(filmsApi);
-
-};
+  await renderSearchPage(filmsApi);
+}
 
 async function renderSearchPage(filmsApi) {
-     try {
-        if (filmsApi.query === '') {
-            clearMarkup();
-            onFetchError();
-            filmsApi.fetchTrendingMovies().then(movies => {
-                createMarkup(movies);
+  try {
+    if (filmsApi.query === '') {
+      clearMarkup();
+      onFetchError();
+      filmsApi.fetchTrendingMovies().then(movies => {
+        createMarkup(movies);
+      });
+    } else {
+      // filmsApi.fetchMoviesByQuery().then(movies => {
+      //     createMarkup(movies);
 
-            });
-        } 
-        else {
-            // filmsApi.fetchMoviesByQuery().then(movies => {
-            //     createMarkup(movies);
+      // });
 
-            // });
-            
-            const allPromises = await Promise.all([
-                filmsApi.fetchGenres(),    // жанры
-                filmsApi.fetchMoviesByQuery() // фильмы
-            ]);
-            const gnr = allPromises[0]; // жанры
-            const movies = allPromises[1]; // фильмы
-            createMarkup(movies);
-            const genreIdsArray = movies.results.map(card => card.genre_ids);
-            const genreNamesArray = genreIdsArray.map(film => film.map(genre => getNameById(gnr.genres, genre).name));
-            const genreEl = document.querySelectorAll('.film__genre');
-            genreEl.forEach((el, ind) => el.textContent = genreNamesArray[ind].splice(0, 2).join(', '));
-        }
-    } catch (error) {
-        console.log(error);
+      const allPromises = await Promise.all([
+        filmsApi.fetchGenres(), // жанры
+        filmsApi.fetchMoviesByQuery(), // фильмы
+      ]);
+      const gnr = allPromises[0]; // жанры
+      const movies = allPromises[1]; // фильмы
+      createMarkup(movies);
+      const genreIdsArray = movies.results.map(card => card.genre_ids);
+      const genreNamesArray = genreIdsArray.map(film =>
+        film.map(genre => getNameById(gnr.genres, genre).name),
+      );
+      const genreEl = document.querySelectorAll('.film__genre');
+      genreEl.forEach((el, ind) => (el.textContent = genreNamesArray[ind].splice(0, 2).join(', ')));
     }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function clearMarkup() {
-    refs.cardSetEl.innerHTML = '';
-};
+  refs.cardSetEl.innerHTML = '';
+}
 
 function onFetchError() {
-    alert({
-      text: 'Search field is empty. Please enter the movie name!',
-    });
+  alert({
+    text: 'Поле поиска фильма пустое! Пожалуйста, введите название фильма',
+  });
 }
- 
-
 
 function getNameById(arr, id) {
-    return arr.find(x => x.id === id);
-} 
+  return arr.find(x => x.id === id);
+}
