6ce35dad84c2aa5a6550091141b5d48814e29382
Oleksandr Simakov <simakov.mailbox@gmail.com>
2021-06-18 14:33:56 +0300

    adds

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,14 +1,48 @@
+const BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = '925491183043d455ae6efbd6833f46c6';
 
 export default class MoviesApi {
-
-
-            console.log(error)
+    constructor() {
+        this.searchQuery = '';
+        this.page = 1;
+        this.totalResults = null;
+        this.currentRequest = null;
     }
-  }
+    
 
+    async fetchTrendingMovies() {
+        this.currentRequest = 'trending';
+        const url = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&page=${this.page}&language=ru`;
 
+        try {
+            const response = await fetch(url);
+            const movies = await response.json();
+            this.totalResults = movies.total_results;
+            this.currentRequest = 'trending';
+            return movies;
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
+    //---genres---
+    async fetchGenres() {
+        // const url = `${BASE_URL}/trending/movie/week?api_key=${API_KEY}&language=ru`;
+        const url = `${BASE_URL}/genre/movie/list?api_key=${API_KEY}&language=ru`;
+
+        try {
+            const response = await fetch(url);
+            const genres = response.json();
+            return genres;
+        } catch (error) {
+            console.log(error)
+        }
+    }    
+    //------------
+
+    async fetchMoviesByQuery() {
+        this.currentRequest = 'search';
+        const url = `${BASE_URL}/search/movie?api_key=${API_KEY}&language=ru&page=${this.page}&include_adult=false&query=${this.searchQuery}`;
 
 <<<<<<< HEAD
     try {
